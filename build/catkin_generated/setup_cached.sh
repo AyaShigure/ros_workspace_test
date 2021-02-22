2158c55d9ec827425ce17b2d9ed4bbcc46cf45ef
AyaShigure <mr.lasky@icoud.com>
2021-02-22 20:44:20 +0900

    initiation of a test ros workspace

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/ubuntu/ros_workspace_test/devel:$CMAKE_PREFIX_PATH"
+export PATH='/opt/ros/noetic/bin:/home/ubuntu/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+export PWD='/home/ubuntu/ros_workspace_test/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/ubuntu/ros_workspace_test/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/ubuntu/ros_workspace_test/src:$ROS_PACKAGE_PATH"
